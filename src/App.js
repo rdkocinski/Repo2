2fe565626367ae9ad0dbab890a11ae304eb6c84a
Dawid Kociński <dawid.kocinski@testto.com>
2026-03-06 07:50:25 +0100

    Now chage repo1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Thats all
+          Edit <code>src/App.js</code> and save to reload. Thats all. Lovabble change
         </p>
         <a
           className="App-link"
